948aacf8c73bad6da262e0c0b2e525801fc5d5f1
Supiri <isalap99@gmail.com>
2017-06-11 00:00:03 +0530

    Minor optimization

--- downloader.py
+++ downloader.py
@@ -12,30 +12,15 @@ def downloader():
 			
 			for row in read_from_db("queue"):
 				if row[2] == "yt-mp4":
-					try:
-						run_cmd('youtube-dl '+row[3]+' -o "'+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s" -f 22 --max-filesize 250m -c -w --no-progress')
-					except Exception as e:
-						logger.error(str(type(e).__name__)+" : "+str(e))
+					run_cmd('youtube-dl '+row[3]+' -o "'+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s" -f 22 --max-filesize 250m -c -w --no-progress')
 					if ("File is larger than max-filesize" in out):
-						try:
-							run_cmd('youtube-dl '+row[3]+' -o "'+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s" -f 18 --max-filesize 250m -c -w --no-progress')
-						except Exception as e:
-							logger.error(str(type(e).__name__)+" : "+str(e))
+						run_cmd('youtube-dl '+row[3]+' -o "'+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s" -f 18 --max-filesize 250m -c -w --no-progress')
 				elif row[2] == "yt-mp3":
-					try:
-						run_cmd('youtube-dl '+row[3]+' -o "'+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s" -f 140 -c -w --no-progress')
-					except Exception as e:
-						logger.error(str(type(e).__name__)+" : "+str(e))
+					run_cmd('youtube-dl '+row[3]+' -o "'+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s" -f 140 -c -w --no-progress')
 				elif row[2] == "yt-pl":
-					try:
-						run_cmd('youtube-dl -o "'+row[6].replace(" ","_")+'%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"  '+row[3]+' -f 22 -c -w --no-progress')
-					except Exception as e:
-						logger.error(str(type(e).__name__)+" : "+str(e))
+					run_cmd('youtube-dl -o "'+row[6].replace(" ","_")+'%(playlist)s/%(playlist_index)s - %(title)s.%(ext)s"  '+row[3]+' -f 22 -c -w --no-progress')
 				elif row[2] == "wget":
-					try:
-						run_cmd("wget "+row[3]+" -P "+row[6]+" "+row[4])
-					except Exception as e:
-						logger.error(str(type(e).__name__)+" : "+str(e))
+					run_cmd("wget "+row[3]+" -P "+row[6]+" "+row[4])
 				logger.debug('Moving the row from queue table to done table')
 				movetodone(row[1],row[2],row[3],row[4],row[5],row[6])
 			#closedb()
