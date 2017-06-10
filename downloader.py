18b39c08ab0689ad7a01ac1dcaa6bfd2828e8821
mrsupiri <isalap99@gmail.com>
2017-06-10 09:29:49 +0530

    New logging system was implement

--- downloader.py
+++ downloader.py
@@ -2,6 +2,7 @@ import datetime
 import time
 import sqlite3
 from common import *
+import logging
 
 out = " "
 err	= " "
@@ -9,29 +10,42 @@ err	= " "
 
 	
 def downloader():
-	log("Info - Waiting 1 mins to Start the Downloader")
 	time.sleep(60)
-	log("Info - Starting Downloader")
+	logger.debug("Starting Downloader")
 	while 1:
 		if int(datetime.now().strftime('%H')) < 8:
-			log("Downloader - Connecting to Database to get Queue")
-			connecttodb()
+			
 			for row in read_from_db("queue"):
 				if row[2] == "yt-mp4":
-					runcmd('youtube-dl '+row[3]+' -o '+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s -f 22 --max-filesize 250m -c -w --no-progress')
+					try:
+						runcmd('youtube-dl '+row[3]+' -o '+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s -f 22 --max-filesize 250m -c -w --no-progress')
+					except Exception as e:
+						logger.error(str(type(e).__name__)+" : "+str(e))
 					if ("File is larger than max-filesize" in out):
-						runcmd('youtube-dl '+row[3]+' -o "'+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s -f 18 --max-filesize 250m -c -w --no-progress')
+						try:
+							runcmd('youtube-dl '+row[3]+' -o "'+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s -f 18 --max-filesize 250m -c -w --no-progress')
+						except Exception as e:
+							logger.error(str(type(e).__name__)+" : "+str(e))
 				elif row[2] == "yt-mp3":
-					runcmd('youtube-dl '+row[3]+' -o '+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s -f 140 -c -w --no-progress')
+					try:
+						runcmd('youtube-dl '+row[3]+' -o '+row[6].replace(" ","_")+row[1].replace(" ","-")+'.%(ext)s -f 140 -c -w --no-progress')
+					except Exception as e:
+						logger.error(str(type(e).__name__)+" : "+str(e))
 				elif row[2] == "wget":
-					runcmd("wget "+row[3]+" -P "+row[6]+" "+row[4])
+					try:
+						runcmd("wget "+row[3]+" -P "+row[6]+" "+row[4])
+					except Exception as e:
+						logger.error(str(type(e).__name__)+" : "+str(e))
+				logger.debug('Moving the row from queue table to done table')
 				movetodone(row[1],row[2],row[3],row[4],row[5],row[6])
-			closedb()
-			log("Downloader - Connection Closed")
-			log("Downloader - Going for 10 Mins Sleep")
+			#closedb()
+			#logger.debug("Connection Closed")
+			logger.info("Waiting 10 mins for next session")
 			time.sleep(300)
 		else:
-			log("Downloader - Wait till the peek off package is on")
+			logger.info("Wait till the peek off package is on")
 			while int(datetime.now().strftime('%H')) >= 8:
 				time.sleep(60)
-				
+
+if __name__=='__main__':
+	downloader()
