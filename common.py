61b6189959b740de95006a36b054f15c8f19eae3
mrsupiri <isalap99@gmail.com>
2017-06-10 09:28:56 +0530

    New logging system was implement

--- common.py
+++ common.py
@@ -3,63 +3,109 @@ import subprocess
 from datetime import datetime
 import time
 import sqlite3
+import logging
 
 global out
 global err
 
 def read_from_db(name):
-    c.execute('SELECT * FROM '+name)
-    return c.fetchall()
-	
+	logger.debug("Reading Data from "+name)
+	try:
+		c.execute('SELECT * FROM '+name)
+		return c.fetchall()
+	except Exception as e:
+		logger.critical(str(type(e).__name__)+" : "+str(e))
+		
 def already_downloaded(url):
 	for row in read_from_db("done"):
 		if url in row:
+			logger.debug(url+"is ready in the downloaded list")
 			return False
 			break
 	for row in read_from_db("queue"):
 		if url in row:
+			logger.debug(url+"is ready in the queue")
 			return False
 			break
 	return True
-
-	
-def log(text):
-	with open("log/yt.log", "a") as f:
-		f.write(datetime.now().strftime('%X %x')+" - "+text+"\n")
-	print(datetime.now().strftime('%X %x')+" - "+text+"\n")
 	
 def runcmd(cmd):
 	p = subprocess.Popen(cmd.split(),stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	out, err = p.communicate()
-	log("cmd - "+cmd)
-	#log("err - "+err)
+	logger.info('executing cmd - '+cmd)
 	for line in str(out).split("\\n"):
-		log("out - "+line)
-	for line in str(err).split("\\n"):
-		log("err - "+line)	
+		if line != "'":
+			logger.info("output - "+line)
+	if err != b'':
+		logger.warn("There was a error while running following command\t\t"+cmd)
+		for line in str(err).split("\\n"):
+			if line != "'":
+				logger.error(line)	
 	
 def movetodone(name,method,url,opt,added_time,path):
 	time = datetime.now().strftime('%X %x')
-	c.execute("INSERT INTO done (name, method, url, opt, added_time, downloaded_time, path) VALUES (?, ?, ?, ?, ?, ?, ?)",
-		(name, method, url, opt, added_time, time, path))
-	conn.commit()
-	c.execute("DELETE FROM queue WHERE url = '"+url+"'")
-	conn.commit()
-		
-
+	logger.debug('INSERT INTO done (name, method, url, opt, added_time, downloaded_time, path) VALUES (%s, %s, %s, %s, %s, %s, %s)' % (name, method, url, opt, added_time, time, path))
+	try:
+		c.execute("INSERT INTO done (name, method, url, opt, added_time, downloaded_time, path) VALUES (?, ?, ?, ?, ?, ?, ?)",
+			(name, method, url, opt, added_time, time, path))
+		conn.commit()
+	except Exception as e:
+		logger.error(str(type(e).__name__)+" : "+str(e))
 	
+	logger.debug("DELETE FROM queue WHERE url = '"+url+"'")
+	try:	
+		c.execute("DELETE FROM queue WHERE url = '"+url+"'")
+		conn.commit()	
+	except Exception as e:
+		logger.error(str(type(e).__name__)+" : "+str(e))
+
 def addtoqueue(name,method,url,path,opt):
 	time = datetime.now().strftime('%X %x')
-	c.execute("INSERT INTO queue (name, method, url, opt, added_time, path) VALUES (?, ?, ?, ?, ?, ?)",
-		(name, method, url, opt, time, path))
+	logger.debug('INSERT INTO queue (name, method, url, opt, added_time, path) VALUES (%s, %s, %s, %s, %s, %s)' % (name, method, url, opt, time, path))
+	try:
+		c.execute("INSERT INTO queue (name, method, url, opt, added_time, path) VALUES (?, ?, ?, ?, ?, ?)",
+			(name, method, url, opt, time, path))
+		conn.commit()
+	except Exception as e:
+		logger.critical(str(type(e).__name__)+" : "+str(e))
+	
+def addyter(name, id, type):
+	c.execute("INSERT INTO youtubers (youtuber, youtube_id, type) VALUES (?, ?, ?)",
+		(name, id, type))
 	conn.commit()
 	
 def connecttodb():
 	global conn
 	global c
-	conn = sqlite3.connect('database/super-dl.db')
-	c = conn.cursor()
+	try:
+		conn = sqlite3.connect('database/super-dl.db')
+		c = conn.cursor()
+	except Exception as e:
+		logger.critical(str(type(e).__name__)+" : "+str(e))
 
 def closedb():
-	c.close
-	conn.close()
+	try:
+		c.close
+		conn.close()
+	except Exception as e:
+		logger.error(str(type(e).__name__)+" : "+str(e))
+		
+	
+global logger
+logger = logging.getLogger("Logger")
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+ch = logging.FileHandler('logs/super-dl.log')
+
+# create formatter
+formatter = logging.Formatter(fmt='%(asctime)-10s %(levelname)-10s: %(module)-10s:  %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+#%(asctime)s  %(name)s\t
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
